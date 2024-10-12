4eae9539d12b08960fe39a58bfe8426fcc0f4adf
Your Name <your.email@example.com>
2024-10-12 20:56:33 +0500

    Initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from "./Navbar/page";
+import Footer from "./Footer/page";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,8 +22,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body >
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
